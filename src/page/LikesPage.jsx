ba68a76196a80a4aa10daee37e8ac023e0e8d338
AHMAD Ella <ellaahmad533@gmail.com>
2023-11-23 11:58:05 +0100

    le message

--- src/page/LikesPage.jsx
+++ src/page/LikesPage.jsx
@@ -2,15 +2,30 @@ import { useState } from "react";
 
 function LikesPage() {
   const [likesCount, setLikesCount] = useState(0);
-
+  const [displayMessage, setDisplayMessage] = useState(false);
   const clickHandle = () => {
-    setLikesCount(likesCount + 1);
+    if(likesCount<5){
+        setLikesCount(likesCount +1);
+
+    }else {
+        setDisplayMessage(true);
+    }
+    
+  };
+  const handleMessageChange =()=>{
+    setDisplayMessage(false)
   };
 // in the first likesCount = 0 
 //setLikes will do the changed that i want 
-//after that useState will keep my last changed and will do the loop again 
+//after that useState will keep my last changed and will do the html render again
   return (
     <main>
+        {displayMessage && (
+           <div>
+             <p>Vous ne pouvez pas liker plus de 5 fois</p>
+            <button on onClick={handleMessageChange}>Fermer le message</button>
+           </div>
+        )}
       <button onClick={clickHandle}>Like</button>
       <p>Vous avez liké {likesCount} fois</p>
     </main>
